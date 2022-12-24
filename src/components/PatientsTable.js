7fad0a943e765c948b90386b11de0b0b71848971
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-23 17:12:39 -0800

    Initialize table

---
+++ src/components/PatientsTable.js
@@ -0,0 +1,29 @@
+import { Box, Table, TableContainer, Tbody, Th, Thead, Tr, Td } from "@chakra-ui/react";
+
+function PatientsTable() {
+    return (
+        <Box height="auto" overflowY="scroll">
+            <TableContainer>
+                <Table>
+                    <Thead>
+                        <Tr>
+                            <Th>First Name</Th>
+                            <Th>Last Name</Th>
+                            <Th>Sex</Th>
+                            <Th>Date of Birth</Th>
+                            <Th>Phone Number</Th>
+                            <Th>Address</Th>
+                            <Th>Actions</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
+        
+    );
+}
+
+export default PatientsTable;
