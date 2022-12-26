1ff9a7945cc8a8fe9bf4bc4a53b7c4f2587cb017
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-25 18:08:57 -0800

    Build Routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import PatientsDasboard from "./pages/PatientsDashboard";
+import PatientDetail from "./pages/PatientDetail";
+import AppointmentsDashboard from './pages/AppointmentsDashboard';
+import DentistsDashboard from "./pages/DentistsDashboard";
+import Dashboard from './pages/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<ChakraProvider>
-		<App />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />} >
+						<Route path="" element={<Dashboard />}/>
+						<Route path="patients" element={<PatientsDasboard />} />
+						<Route path="patients/:id" element={<PatientDetail />} />
+						<Route path="dentists" element={<DentistsDashboard />} />
+						<Route path="appointments" element={<AppointmentsDashboard />}/>
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</ChakraProvider>
 	</React.StrictMode>
 );
