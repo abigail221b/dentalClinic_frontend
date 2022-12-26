db1ed74d5bbcbb75b49b9ea51a5395f27ad624b2
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-25 17:34:24 -0800

    Create Dentist card component

---
+++ src/components/DentistCard.js
@@ -0,0 +1,17 @@
+import { Card, CardHeader, CardBody, CardFooter, Image, Heading, Divider, Button } from '@chakra-ui/react'
+
+function DentistCard() {
+    return (
+        <Card>
+            <CardBody>
+                <Image src="https://via.placeholder.com/200" borderRadius="lg" />
+                <Heading size='md'>Dentist 1</Heading>
+            </CardBody>
+            <CardFooter>
+                <Button>Details</Button>
+            </CardFooter>
+        </Card>
+    );
+}
+
+export default DentistCard;
