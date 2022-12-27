ced6927e56549982aa07034c47d6de7a2fccaf1d
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-27 14:20:05 -0800

    Render form and table components on the dashboard

--- src/pages/AppointmentsDashboard.js
+++ src/pages/AppointmentsDashboard.js
@@ -1,82 +1,15 @@
-import { Flex, Box, Heading, Stack, Button, HStack, Text } from "@chakra-ui/react";
-import {
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-} from '@chakra-ui/react';
-import {
-    FormControl,
-    FormLabel,
-    Input,
-    Checkbox,
-    CheckboxGroup,
-    RadioGroup,
-    Radio
-    } from '@chakra-ui/react';
-import { useState } from "react";
-    
+import { Flex, Heading } from "@chakra-ui/react";
+
+import AppointmentSearchForm from "../components/AppointmentSearchForm";
+import AppointmentsTable from "../components/AppointmentsTable";
 
 function AppontmentsDashboard() {
-
-    const [dateSearchBy, setDateSearchBy] = useState("date")
 
     return (
         <Flex flexDirection="column" width="100%" height="100vh">
             <Heading>Appointments</Heading>
-            <Box>
-                <Heading size="md">Search Appointments</Heading>
-                <FormControl as="fieldset">
-                    <RadioGroup onChange={setDateSearchBy} value={dateSearchBy}>
-                        <HStack>
-                            <FormControl>
-                                <Radio value="date">Search by date</Radio>
-                                <Input type="date"  isDisabled={ dateSearchBy !== "date"} />
-                            </FormControl>
-                            <FormControl>
-                                <Radio value="date-range">Search by date range</Radio>
-                                <HStack>
-                                    <Input type="date"  isDisabled={ dateSearchBy !== "date-range"}/>
-                                    <Text> to </Text>
-                                    <Input type="date"  isDisabled={ dateSearchBy !== "date-range"}/>
-                                </HStack>
-                            </FormControl>
-                        </HStack>
-                    </RadioGroup>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Dentist</FormLabel>
-                    <CheckboxGroup colorScheme='green' defaultValue={['dentist1']}>
-                        <HStack>
-                            <Checkbox value='dentist1'>dentist1</Checkbox>
-                            <Checkbox value='dentist2'>dentist2</Checkbox>
-                            <Checkbox value='dentist3'>dentist3</Checkbox>
-                        </HStack>
-                    </CheckboxGroup>
-                </FormControl>
-                <Button>Search</Button>
-            </Box>
-            <TableContainer>
-                <Table>
-                    <Thead>
-                        <Tr>
-                            <Th>First Name</Th>
-                            <Th>Last Name</Th>
-                            <Th>Date</Th>
-                            <Th>Time</Th>
-                            <Th>Duration</Th>
-                            <Th>Dentist</Th>
-                            <Th>Actions</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        
-                    </Tbody>
-                </Table>
-            </TableContainer>
+            <AppointmentSearchForm />
+            <AppointmentsTable />
         </Flex>
     );
 }
