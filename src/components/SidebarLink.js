3bcd9e90f5b6d73c99466641abcc1c8a3416afb6
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-27 12:48:41 -0800

    Create component

---
+++ src/components/SidebarLink.js
@@ -0,0 +1,24 @@
+import { LinkBox, LinkOverlay, HStack, Box, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
+
+function SidebarLink({ dashboard, icon, url, selectedDashboard, setSelectedDashboard, styles }) {
+    return (
+        <LinkBox 
+            as={ RouterLink } 
+            to={ url }
+            onClick={() => setSelectedDashboard(dashboard)} 
+            style={{...styles.link, ...(selectedDashboard === dashboard && styles.selectedLink )}}>
+            <LinkOverlay>
+                <HStack>
+                    <Box style={ styles.icon }>
+                        { icon }
+                    </Box>
+                    <Text>{ dashboard }</Text>
+                </HStack>
+            </LinkOverlay>
+        </LinkBox>
+    );
+}
+
+export default SidebarLink;
