738eb5803a3a63568ebc5a71d4927f698ef830fd
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-05 13:39:54 -0800

    Add patients state to dashboard

--- src/pages/PatientsDashboard.js
+++ src/pages/PatientsDashboard.js
@@ -1,8 +1,12 @@
 import { Flex, Heading } from "@chakra-ui/react";
+import { useState } from "react";
 import PatientSearchForm from "../components/PatientSearchForm";
 import PatientsTable from "../components/PatientsTable";
 
 function PatientsDashboard() {
+
+    const [patients, setPatients] = useState([]);
+
     return (
         <Flex flexDirection="column" width="100%" height="100vh">
             <Heading>Patients</Heading>
