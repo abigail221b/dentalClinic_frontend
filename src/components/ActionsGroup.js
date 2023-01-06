311cab76a296ecbdaea755a34cc59b4cae93bdd7
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-05 17:50:55 -0800

    Show NewAppointmentForm Modal onClick

--- src/components/ActionsGroup.js
+++ src/components/ActionsGroup.js
@@ -2,18 +2,22 @@ import { BsPersonLinesFill } from "react-icons/bs";
 import { IoMdAdd } from "react-icons/io";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { Flex, Tooltip, IconButton } from "@chakra-ui/react";
+import { useDisclosure } from '@chakra-ui/react'
+import NewAppointmentForm from "../pages/NewAppointmentForm";
 
 function ActionsGroup({ patient }) {
-function ActionsGroup() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
         <Flex gap="5">
             <Tooltip label="Book Appointment">
-                <IconButton size="md" icon={<IoMdAdd />} />
+                <IconButton onClick={onOpen} size="md" icon={<IoMdAdd />} />
             </Tooltip>
+            <NewAppointmentForm isOpen={isOpen} onClose={onClose} patient={patient}/>
 
             <Tooltip label="Patient Details">
-                <IconButton size="md" icon={<BsPersonLinesFill />} />
+                <IconButton  size="md" icon={<BsPersonLinesFill />} />
             </Tooltip>
+            
 
             <Tooltip label="Delete patient">
                 <IconButton size="md" icon={<RiDeleteBinFill />} />
