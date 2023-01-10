6f2ddf00409b6b7267bea787596e50ae051b7f4a
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 12:45:33 -0800

    Create New Dentist form

---
+++ src/pages/NewDentistForm.js
@@ -0,0 +1,32 @@
+import { Heading, Flex, FormControl, FormLabel, Input, HStack, Button, RadioGroup, Radio } from "@chakra-ui/react";
+
+function NewDentistForm() {
+    return (
+        <Flex flexDirection="column" width="100%" height="100vh" padding="25px" gap="3">
+            <Heading>New Dentist</Heading>
+                <HStack>
+                    <FormControl>
+                        <FormLabel>First Name</FormLabel>
+                        <Input type="text" />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Last Name</FormLabel>
+                        <Input type="text" />
+                    </FormControl>
+                </HStack>
+                <HStack>
+                    <FormControl>
+                        <FormLabel>Phone Number</FormLabel>
+                        <Input type="tel" />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Email</FormLabel>
+                        <Input type="email" />
+                    </FormControl>
+                </HStack>
+                <Button width="100px">Submit</Button>
+        </Flex>
+    );
+}
+
+export default NewDentistForm;
