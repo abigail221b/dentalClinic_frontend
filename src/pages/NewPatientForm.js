ce4abf25404dbb5299daad6150f3ad41714fb4a0
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 14:53:13 -0800

    Show invalid inputs onSubmit

--- src/pages/NewPatientForm.js
+++ src/pages/NewPatientForm.js
@@ -10,8 +10,16 @@ function NewPatientForm() {
     const [phoneNumber, setPhoneNumber] = useState("");
     const [email, setEmail] = useState("");
     const [address, setAddress] = useState("");
+    const [formError, setFormError] = useState(false);
 
     const handleSubmit = () => {
+
+        if(firstName === "" || lastName === "" || sex === "" || dateOfBirth === "" || 
+            phoneNumber === "" || email === "" || address === "") {
+            setFormError(true);
+            return;
+        }
+
         fetch(`http://localhost:8080/patients`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -31,17 +39,17 @@ function NewPatientForm() {
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px" gap="3">
             <Heading>New Patient</Heading>
                 <HStack>
-                    <FormControl>
+                    <FormControl isInvalid={formError && firstName === ""}>
                         <FormLabel>First Name</FormLabel>
                         <Input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
                     </FormControl>
-                    <FormControl>
+                    <FormControl isInvalid={formError && lastName === ""}>
                         <FormLabel>Last Name</FormLabel>
                         <Input type="text" value={lastName} onChange={e => setLastName(e.target.value)}  />
                     </FormControl>
                 </HStack>
                 <HStack>
-                    <FormControl>
+                    <FormControl isInvalid={formError && sex === ""}>
                         <FormLabel>Sex</FormLabel>
                         <RadioGroup onChange={setSex}>
                             <HStack>
@@ -50,22 +58,22 @@ function NewPatientForm() {
                             </HStack>
                         </RadioGroup>
                     </FormControl>
-                    <FormControl>
+                    <FormControl isInvalid={formError && dateOfBirth === ""}>
                         <FormLabel>Date of Birth</FormLabel>
                         <Input type="date" value={dateOfBirth} onChange={e => setDateOfBirth(e.target.value)} />
                     </FormControl>
                 </HStack>
                 <HStack>
-                    <FormControl>
+                    <FormControl isInvalid={formError && phoneNumber === ""}>
                         <FormLabel>Phone Number</FormLabel>
                         <Input type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
                     </FormControl>
-                    <FormControl>
+                    <FormControl isInvalid={formError && email === ""}>
                         <FormLabel>Email</FormLabel>
                         <Input type="email" value={email} onChange={e => setEmail(e.target.value)} />
                     </FormControl>
                 </HStack>
-                <FormControl>
+                <FormControl isInvalid={formError && address === ""}>
                     <FormLabel>Address</FormLabel>
                     <Input type="text" value={address} onChange={e => setAddress(e.target.value)} />
                 </FormControl>
