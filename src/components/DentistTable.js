b1fb5815e883b0fe1514f1de089c86ddbe18b8e4
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 10:16:39 -0800

    Create component to render dentists in a Table

---
+++ src/components/DentistTable.js
@@ -0,0 +1,31 @@
+import { Box, Table, TableContainer, Tbody, Th, Thead, Tr, Td } from "@chakra-ui/react";
+import DentistActionsGroup from "./DentistActionsGroup";
+
+function DentistTable({ dentists }) {
+    return (
+        <Box width="100%" height="auto" overflowY="scroll">
+            <TableContainer width="100%">
+                <Table>
+                    <Thead>
+                        <Tr>
+                            <Th>First Name</Th>
+                            <Th>Last Name</Th>
+                            <Th>Phone Number</Th>
+                            <Th>Actions</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td></Td>
+                            <Td></Td>
+                            <Td></Td>
+                            <Td><DentistActionsGroup /></Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
+    );
+}
+
+export default DentistTable;
