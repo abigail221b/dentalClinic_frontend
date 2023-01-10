5ec881e87b69dfe11e84df810a23cb8a5bd60d0d
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 10:32:32 -0800

    Add New Dentist button

--- src/pages/DentistsDashboard.js
+++ src/pages/DentistsDashboard.js
@@ -5,9 +5,10 @@ import DentistTable from "../components/DentistTable";
 function DentistsDashboard() {
     return (
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px">
-        <Flex flexDirection="column" width="100%" height="100vh">
-            <Heading>Dentists</Heading>
-            <Button maxWidth="150px">New Dentist</Button>
+            <Flex justifyContent="space-between">
+                <Heading>Dentists</Heading>
+                <Button>New Dentist</Button>
+            </Flex>
             <Flex gap="10">
                 <DentistTable />
             </Flex>
