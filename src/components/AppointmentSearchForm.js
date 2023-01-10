3a58df57b9413dda52cf890e7b1f35cbcbcefb57
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 11:44:50 -0800

    Style form as a Card

--- src/components/AppointmentSearchForm.js
+++ src/components/AppointmentSearchForm.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Button, HStack, Text } from "@chakra-ui/react";
+import { Flex, Heading, Button, HStack, Text, Card, Stack } from "@chakra-ui/react";
 import {
     FormControl,
     FormLabel,
@@ -15,38 +15,40 @@ function AppointmentSearchForm() {
     const [dateSearchBy, setDateSearchBy] = useState("date");
 
     return (
-        <Box>
-            <Heading size="md">Search Appointments</Heading>
-            <FormControl as="fieldset">
-                <RadioGroup onChange={setDateSearchBy} value={dateSearchBy}>
-                    <HStack>
-                        <FormControl>
-                            <Radio value="date">Search by date</Radio>
-                            <Input type="date"  isDisabled={ dateSearchBy !== "date"} />
-                        </FormControl>
-                        <FormControl>
-                            <Radio value="date-range">Search by date range</Radio>
-                            <HStack>
-                                <Input type="date"  isDisabled={ dateSearchBy !== "date-range"}/>
-                                <Text> to </Text>
-                                <Input type="date"  isDisabled={ dateSearchBy !== "date-range"}/>
-                            </HStack>
-                        </FormControl>
-                    </HStack>
-                </RadioGroup>
-            </FormControl>
-            <FormControl>
-                <FormLabel>Dentist</FormLabel>
-                <CheckboxGroup colorScheme='green' defaultValue={['dentist1']}>
-                    <HStack>
-                        <Checkbox value='dentist1'>dentist1</Checkbox>
-                        <Checkbox value='dentist2'>dentist2</Checkbox>
-                        <Checkbox value='dentist3'>dentist3</Checkbox>
-                    </HStack>
-                </CheckboxGroup>
-            </FormControl>
-            <Button>Search</Button>
-        </Box>
+        <Card padding="25px" margin="10px 0">
+            <Stack gap="2">
+                <Heading size="md">Search Appointments</Heading>
+                <FormControl as="fieldset">
+                    <RadioGroup onChange={setDateSearchBy} value={dateSearchBy}>
+                        <HStack>
+                            <FormControl>
+                                <Radio value="date">Search by date</Radio>
+                                <Input type="date"  isDisabled={ dateSearchBy !== "date"} />
+                            </FormControl>
+                            <FormControl>
+                                <Radio value="date-range">Search by date range</Radio>
+                                <HStack>
+                                    <Input type="date"  isDisabled={ dateSearchBy !== "date-range"}/>
+                                    <Text> to </Text>
+                                    <Input type="date"  isDisabled={ dateSearchBy !== "date-range"}/>
+                                </HStack>
+                            </FormControl>
+                        </HStack>
+                    </RadioGroup>
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Dentist</FormLabel>
+                    <CheckboxGroup colorScheme='green' defaultValue={['dentist1']}>
+                        <HStack>
+                            <Checkbox value='dentist1'>dentist1</Checkbox>
+                            <Checkbox value='dentist2'>dentist2</Checkbox>
+                            <Checkbox value='dentist3'>dentist3</Checkbox>
+                        </HStack>
+                    </CheckboxGroup>
+                </FormControl>
+                <Button width="100px">Search</Button>
+            </Stack>
+        </Card>
     );
 }
 
