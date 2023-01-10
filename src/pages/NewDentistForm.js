a016f61697601949d0cdc45b2367215ff386e25a
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 15:07:51 -0800

    Check for an empty form onSubmit

--- src/pages/NewDentistForm.js
+++ src/pages/NewDentistForm.js
@@ -7,31 +7,38 @@ function NewDentistForm() {
     const [lastName, setLastName] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
     const [email, setEmail] = useState("");
+    const [formError, setFormError] = useState(false);
+
+    const handleSubmit = () => {
+        if(firstName === "" || lastName === "" || phoneNumber === "" || email === "") {
+            setFormError(true);
+        }
+    }
 
     return (
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px" gap="3">
             <Heading>New Dentist</Heading>
                 <HStack>
-                    <FormControl>
+                    <FormControl isInvalid={formError && firstName === ""}>
                         <FormLabel>First Name</FormLabel>
                         <Input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
                     </FormControl>
-                    <FormControl>
+                    <FormControl isInvalid={formError && lastName === ""}>
                         <FormLabel>Last Name</FormLabel>
                         <Input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
                     </FormControl>
                 </HStack>
                 <HStack>
-                    <FormControl>
+                    <FormControl isInvalid={formError && phoneNumber === ""}>
                         <FormLabel>Phone Number</FormLabel>
                         <Input type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
                     </FormControl>
-                    <FormControl>
+                    <FormControl isInvalid={formError && email === ""}>
                         <FormLabel>Email</FormLabel>
                         <Input type="email" value={email} onChange={e => setEmail(e.target.value)} />
                     </FormControl>
                 </HStack>
-                <Button width="100px">Submit</Button>
+                <Button width="100px" onClick={handleSubmit}>Submit</Button>
         </Flex>
     );
 }
