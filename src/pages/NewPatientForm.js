86f4a2231857cb311fd38ca4ebfbc0442f838c8d
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-10 14:32:07 -0800

    Add form input states

--- src/pages/NewPatientForm.js
+++ src/pages/NewPatientForm.js
@@ -1,6 +1,16 @@
 import { Heading, Flex, FormControl, FormLabel, Input, HStack, Button, RadioGroup, Radio } from "@chakra-ui/react";
+import { useState } from "react";
 
 function NewPatientForm() {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [sex, setSex] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState(null);
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+
     return (
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px" gap="3">
             <Heading>New Patient</Heading>
