fc7debc22f88e2f15dc4611d6f8f986697f6fb6b
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-11 15:09:21 -0800

    Add Modal state to handle UpdatePatientForm

--- src/components/PatientDetailButtonGroup.js
+++ src/components/PatientDetailButtonGroup.js
@@ -6,15 +6,15 @@ import UpdatePatientForm from "../pages/UpdatePatientForm";
 function PatientDetailButtonGroup({ patient, setPatient }) {
 
     const { isOpen: newApptFormIsOpen, onOpen: newApptFormOnOpen, onClose: newApptFormOnClose } = useDisclosure();
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen: updateFormIsOpen, onOpen: updateFormOnOpen, onClose: updateFormOnClose } = useDisclosure();
 
     return (
         <ButtonGroup>
             <Button onClick={newApptFormOnOpen}>Book Appointment</Button>
                 <NewAppointmentForm isOpen={newApptFormIsOpen} onClose={newApptFormOnClose} patient={patient} />
                 
-            <Button onClick={onOpen}>Edit Patient</Button>
-                <UpdatePatientForm isOpen={isOpen} onClose={onClose} patient={patient} setPatient={setPatient} />
+            <Button onClick={updateFormOnOpen}>Edit Patient</Button>
+                <UpdatePatientForm isOpen={updateFormIsOpen} onClose={updateFormOnClose} patient={patient} setPatient={setPatient} />
                 
             <Button>Delete Patient</Button>
         </ButtonGroup>
