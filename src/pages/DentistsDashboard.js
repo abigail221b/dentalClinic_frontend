af6edfeedd6cf4cdf23f5aa94dda3e0020cad93e
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-11 09:36:20 -0800

    Fetch dentists to display

--- src/pages/DentistsDashboard.js
+++ src/pages/DentistsDashboard.js
@@ -1,8 +1,18 @@
 import { Flex, Button, Heading } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import DentistTable from "../components/DentistTable";
 
 function DentistsDashboard() {
+
+    const [dentists, setDentists] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/dentists")
+        .then(res => res.json())
+        .then(dentists => setDentists(dentists));
+    }, []);
+
     return (
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px">
             <Flex justifyContent="space-between">
@@ -10,7 +20,7 @@ function DentistsDashboard() {
                 <Button as={RouterLink} to="/dentists/new">New Dentist</Button>
             </Flex>
             <Flex gap="10">
-                <DentistTable />
+                <DentistTable dentists={dentists}/>
             </Flex>
         </Flex>
     );
