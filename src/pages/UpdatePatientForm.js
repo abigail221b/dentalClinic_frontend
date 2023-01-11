37de06cc971bf4e3f491877828cb80d9f15bdc7c
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-11 12:59:06 -0800

    Transform component into a Modal

--- src/pages/UpdatePatientForm.js
+++ src/pages/UpdatePatientForm.js
@@ -1,5 +1,14 @@
 import { Flex, FormControl, FormLabel, Input, HStack, RadioGroup, Radio, Heading, Button } from "@chakra-ui/react";
 import { useState } from "react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
 
 function UpdatePatientForm({ isOpen, onClose, patientData }) {
 
@@ -14,60 +23,90 @@ function UpdatePatientForm({ isOpen, onClose, patientData }) {
     }
     
     return (
-        <Flex flexDirection="column" width="100%" height="100vh" padding="25px" gap="3">
-            <Heading>Update Patient Information</Heading>
-                <HStack>
-                    <FormControl>
-                        <FormLabel>First Name</FormLabel>
+        <>
+            <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose} size="xl">
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        <Heading>Update Patient Information</Heading>
+                        <ModalCloseButton />
+                    </ModalHeader>
+                    <ModalBody>
+                        <HStack>
+                            <FormControl>
+                                <FormLabel>First Name</FormLabel>
+                                <Input type="text" 
                         <Input type="text" 
+                                <Input type="text" 
+                                    value={patient.firstName} 
                                value={patient.firstName} 
-                               onChange={e => setPatient({...patient, firstName: e.target.value})} />
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Last Name</FormLabel>
+                                    value={patient.firstName} 
+                                    onChange={e => setPatient({...patient, firstName: e.target.value})} />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Last Name</FormLabel>
+                                <Input type="text" 
                         <Input type="text" 
+                                <Input type="text" 
+                                    value={patient.lastName} 
                                value={patient.lastName} 
-                               onChange={e => setPatient({...patient, lastName: e.target.value})} />
-                    </FormControl>
-                </HStack>
-                <HStack>
-                    <FormControl>
-                        <FormLabel>Sex</FormLabel>
-                        <RadioGroup value={patient.sex}
-                                    onChange={value => setPatient({...patient, sex: value})}>
-                            <HStack>
-                                <Radio value="M">M</Radio>
-                                <Radio value="F">F</Radio>
-                            </HStack>
-                        </RadioGroup>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Date of Birth</FormLabel>
+                                    value={patient.lastName} 
+                                    onChange={e => setPatient({...patient, lastName: e.target.value})} />
+                            </FormControl>
+                        </HStack>
+                        <HStack>
+                            <FormControl>
+                                <FormLabel>Sex</FormLabel>
+                                <RadioGroup value={patient.sex}
+                                            onChange={value => setPatient({...patient, sex: value})}>
+                                    <HStack>
+                                        <Radio value="M">M</Radio>
+                                        <Radio value="F">F</Radio>
+                                    </HStack>
+                                </RadioGroup>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Date of Birth</FormLabel>
+                                <Input type="date" 
                         <Input type="date" 
-                               value={patient.dateOfBirth}
-                               onChange={e => setPatient({...patient, dateOfBirth: e.target.value})} />
-                    </FormControl>
-                </HStack>
-                <HStack>
-                    <FormControl>
-                        <FormLabel>Phone Number</FormLabel>
+                                <Input type="date" 
+                                    value={patient.dateOfBirth}
+                                    onChange={e => setPatient({...patient, dateOfBirth: e.target.value})} />
+                            </FormControl>
+                        </HStack>
+                        <HStack>
+                            <FormControl>
+                                <FormLabel>Phone Number</FormLabel>
+                                <Input type="tel" 
                         <Input type="tel" 
+                                <Input type="tel" 
+                                    value={patient.phoneNumber} 
                                value={patient.phoneNumber} 
-                               onChange={e => setPatient({...patient, phoneNumber: e.target.value})} />
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Email</FormLabel>
+                                    value={patient.phoneNumber} 
+                                    onChange={e => setPatient({...patient, phoneNumber: e.target.value})} />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Email</FormLabel>
+                                <Input type="email" 
                         <Input type="email" 
+                                <Input type="email" 
+                                    value={patient.email} 
                                value={patient.email} 
-                               onChange={e => setPatient({...patient, email: e.target.value})} />
-                    </FormControl>
-                </HStack>
-                <FormControl>
-                    <FormLabel>Address</FormLabel>
-                    <Input type="text" value={patient.address} />
-                </FormControl>
-                <Button width="100px" onClick={handleSubmit}>Update</Button>
-        </Flex>
+                                    value={patient.email} 
+                                    onChange={e => setPatient({...patient, email: e.target.value})} />
+                            </FormControl>
+                        </HStack>
+                        <FormControl>
+                            <FormLabel>Address</FormLabel>
+                            <Input type="text" value={patient.address} />
+                        </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button width="100px" onClick={handleSubmit}>Update</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
 
