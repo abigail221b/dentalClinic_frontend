b5d512f26b3ad644a5d21229d338529c5ea032c2
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-11 15:57:48 -0800

    Turn form into a Modal

--- src/pages/UpdateDentistForm.js
+++ src/pages/UpdateDentistForm.js
@@ -1,32 +1,53 @@
 import { Heading, Flex, FormControl, FormLabel, Input, HStack, Button } from "@chakra-ui/react";
 import { useState } from "react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
 
 function UpdateDentistForm({ dentist }) {
     return (
-        <Flex flexDirection="column" width="100%" height="100vh" padding="25px" gap="3">
-            <Heading>Update Dentist</Heading>
-                <HStack>
-                    <FormControl>
-                        <FormLabel>First Name</FormLabel>
-                        <Input type="text" value={dentist.firstName} />
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Last Name</FormLabel>
-                        <Input type="text" value={dentist.lastName} />
-                    </FormControl>
-                </HStack>
-                <HStack>
-                    <FormControl>
-                        <FormLabel>Phone Number</FormLabel>
-                        <Input type="tel" value={dentist.phoneNumber} />
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Email</FormLabel>
-                        <Input type="email" value={dentist.email} />
-                    </FormControl>
-                </HStack>
-                <Button width="100px">Submit</Button>
-        </Flex>
+        <>
+            <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose} size="xl">
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        <Heading>Update Dentist</Heading>
+                        <ModalCloseButton />
+                    </ModalHeader>
+                    <ModalBody>
+                        <HStack>
+                            <FormControl>
+                                <FormLabel>First Name</FormLabel>
+                                <Input type="text" value={dentist.firstName} />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Last Name</FormLabel>
+                                <Input type="text" value={dentist.lastName} />
+                            </FormControl>
+                        </HStack>
+                        <HStack>
+                            <FormControl>
+                                <FormLabel>Phone Number</FormLabel>
+                                <Input type="tel" value={dentist.phoneNumber} />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Email</FormLabel>
+                                <Input type="email" value={dentist.email} />
+                            </FormControl>
+                        </HStack>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button width="100px">Submit</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
 
