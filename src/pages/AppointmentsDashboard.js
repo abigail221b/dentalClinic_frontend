c27c232574e402f069fa0d53be3c115144cfa1bb
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-13 14:46:43 -0800

    Add appointments state to dashboard

--- src/pages/AppointmentsDashboard.js
+++ src/pages/AppointmentsDashboard.js
@@ -4,6 +4,9 @@ import AppointmentsTable from "../components/AppointmentsTable";
 import { Flex, Heading } from "@chakra-ui/react";
 
 function AppontmentsDashboard() {
+
+    const [appointments, setAppointments] = useState([]);
+
     return (
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px" backgroundColor="#F9F9F9">
             <Heading>Appointments</Heading>
