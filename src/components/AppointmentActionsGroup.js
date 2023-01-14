ae956f00d190334ccddd59c8f3b3b7010713c1d7
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-13 17:24:52 -0800

    Turn UpdateAppointmentForm into a Modal

--- src/components/AppointmentActionsGroup.js
+++ src/components/AppointmentActionsGroup.js
@@ -1,13 +1,18 @@
-import { Flex, Tooltip, IconButton } from "@chakra-ui/react";
+import { Flex, Tooltip, IconButton, useDisclosure } from "@chakra-ui/react";
 import { RiEdit2Fill } from "react-icons/ri";
 import { RiDeleteBinFill } from "react-icons/ri";
+import UpdateAppointmentForm from "../pages/UpdateAppointmentForm";
 
 function AppointmentActionsGroup() {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <Flex gap="5">
             <Tooltip label="Update Appointment">
-                <IconButton size="md" icon={<RiEdit2Fill />} />
+                <IconButton onClick={onOpen} size="md" icon={<RiEdit2Fill />} />
             </Tooltip>
+            <UpdateAppointmentForm isOpen={isOpen} onClose={onClose} />
 
             <Tooltip label="Delete Appointment">
                 <IconButton colorScheme="red" size="md" icon={<RiDeleteBinFill />} />
--- src/pages/UpdateAppointmentForm.js
+++ src/pages/UpdateAppointmentForm.js
@@ -1,42 +1,53 @@
 import { NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from "@chakra-ui/react"
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, } from '@chakra-ui/react'
 import { FormControl, FormLabel, Input, RadioGroup, Radio } from '@chakra-ui/react';
 import { Flex, Heading, Text, Button } from "@chakra-ui/react";
 
-function UpdateAppointmentForm() {
+function UpdateAppointmentForm({ isOpen, onClose }) {
     return (
-        <Flex flexDirection="column" width="100%" height="100vh" padding="25px" backgroundColor="#F9F9F9">
-            <Heading>Update Appointment</Heading>
-            <FormControl>
-                <FormLabel>Dentist</FormLabel>
-                <RadioGroup>
-                    <Flex gap="5" wrap="wrap" paddingBottom="20px">
-                        <Radio value="1">Dr. 1</Radio>
-                        <Radio value="2">Dr. 2</Radio>
+        <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose} size="xl">
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>
+                    <Heading>Update Appointment</Heading>
+                    <ModalCloseButton />
+                </ModalHeader>
+                <ModalBody>
+                    <FormControl>
+                        <FormLabel>Dentist</FormLabel>
+                        <RadioGroup>
+                            <Flex gap="5" wrap="wrap" paddingBottom="20px">
+                                <Radio value="1">Dr. 1</Radio>
+                                <Radio value="2">Dr. 2</Radio>
+                            </Flex>
+                        </RadioGroup>
+                    </FormControl>
+                    <Flex gap="6">
+                        <FormControl>
+                            <FormLabel>Date</FormLabel>
+                            <Input type="date" />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Time</FormLabel>
+                            <Input type="time" />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Duration</FormLabel>
+                            <NumberInput defaultValue={45} min={15} max={120} step={15}>
+                                <NumberInputField />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </FormControl>
                     </Flex>
-                </RadioGroup>
-            </FormControl>
-            <Flex gap="6">
-                <FormControl>
-                    <FormLabel>Date</FormLabel>
-                    <Input type="date" />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Time</FormLabel>
-                    <Input type="time" />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Duration</FormLabel>
-                    <NumberInput defaultValue={45} min={15} max={120} step={15}>
-                        <NumberInputField />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                </FormControl>
-            </Flex>
-            <Button width="100px" colorScheme="teal">Submit</Button>
-        </Flex>
+                </ModalBody>
+                <ModalFooter>
+                    <Button width="100px" colorScheme="teal">Submit</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
     );
 }
 
