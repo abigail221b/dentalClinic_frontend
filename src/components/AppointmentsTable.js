7924787608d2c63d51f75e2ba1603effbbf7ca2d
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-18 17:20:25 -0800

    Show or hide Patient name on AppointmentsTable

--- src/components/AppointmentsTable.js
+++ src/components/AppointmentsTable.js
@@ -1,14 +1,17 @@
 import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
 import AppointmentActionsGroup from './AppointmentActionsGroup';
 
-function AppointmentsTable({ appointments }) {
+function AppointmentsTable({ appointments, displayPatientName }) {
     return (
         <TableContainer backgroundColor="white">
                 <Table>
                     <Thead>
                         <Tr>
-                            <Th>First Name</Th>
-                            <Th>Last Name</Th>
+                            {displayPatientName &&
+                                <>
+                                    <Th>First Name</Th>
+                                    <Th>Last Name</Th>
+                                </>}
                             <Th>Date</Th>
                             <Th>Time</Th>
                             <Th>Duration</Th>
@@ -19,8 +22,11 @@ function AppointmentsTable({ appointments }) {
                     <Tbody>
                         {appointments.map(appointment => (
                             <Tr>
-                                <Td>{appointment.patient.firstName}</Td>
-                                <Td>{appointment.patient.lastName}</Td>
+                                {displayPatientName &&
+                                    <>
+                                        <Td>{appointment.patient.firstName}</Td>
+                                        <Td>{appointment.patient.lastName}</Td>
+                                    </>}
                                 <Td>{appointment.date}</Td>
                                 <Td>{appointment.startTime}</Td>
                                 <Td>{appointment.duration}</Td>
