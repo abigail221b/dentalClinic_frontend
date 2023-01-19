14dc54212571a7619f3a74d3cae5b7f594dbeaea
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-18 17:21:02 -0800

    Include prop to show Patient name on table

--- src/pages/AppointmentsDashboard.js
+++ src/pages/AppointmentsDashboard.js
@@ -12,7 +12,9 @@ function AppontmentsDashboard() {
         <Flex flexDirection="column" width="100%" height="100vh" padding="25px" backgroundColor="#F9F9F9">
             <Heading>Appointments</Heading>
             <AppointmentSearchForm setAppointments={setAppointments} />
-            <AppointmentsTable appointments={appointments} />
+            <AppointmentsTable
+                appointments={appointments}
+                displayPatientName={true} />
         </Flex>
     );
 }
