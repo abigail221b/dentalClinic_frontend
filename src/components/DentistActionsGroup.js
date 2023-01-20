9440c64293dbeae46be9a6cb8fd27b7159c12b45
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-20 12:59:52 -0800

    Add DeletePrompt to confirm and handle delete

--- src/components/DentistActionsGroup.js
+++ src/components/DentistActionsGroup.js
@@ -1,4 +1,5 @@
 import UpdateDentistForm from "../pages/UpdateDentistForm";
+import DeletePrompt from "./DeletePrompt";
 
 import { Flex, Tooltip, IconButton, useDisclosure } from "@chakra-ui/react";
 import { RiEdit2Fill, RiDeleteBinFill } from "react-icons/ri";
@@ -8,6 +9,10 @@ function DentistActionsGroup({ dentist, setDentist }) {
     const { isOpen: updateDentistIsOpen,
             onOpen: updateDentistOnOpen,
             onClose: updateDentistOnClose } = useDisclosure();
+
+    const { isOpen: deletePromptIsOpen,
+            onOpen: deletePromptOnOpen,
+            onClose: deletePromptOnClose } = useDisclosure();
 
     return (
         <Flex gap="5">
@@ -21,8 +26,13 @@ function DentistActionsGroup({ dentist, setDentist }) {
                 setDentist={setDentist} />
 
             <Tooltip label="Delete Dentist">
-                <IconButton size="md" colorScheme="red" icon={<RiDeleteBinFill />} />
+                <IconButton onClick={deletePromptOnOpen} size="md" colorScheme="red" icon={<RiDeleteBinFill />} />
             </Tooltip>
+            <DeletePrompt
+                isOpen={deletePromptIsOpen}
+                onClose={deletePromptOnClose}
+                message={"Are you sure you want to delete this dentist?"}
+                endpoint={`http://${process.env.REACT_APP_BACKEND_HOST}:8080/dentists/${dentist.id}`} />
         </Flex>
     );
 }
