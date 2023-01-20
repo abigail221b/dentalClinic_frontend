f96feac2cff2daac63b7d0fd766040bbdfd77ab4
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-20 11:51:04 -0800

    Create popup modal to confirm deletion

---
+++ src/components/DeletePrompt.js
@@ -0,0 +1,26 @@
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, HStack, } from '@chakra-ui/react'
+import { Button, Text } from "@chakra-ui/react";
+
+function DeletePrompt({ isOpen, onClose, message }) {
+    return (
+        <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose} size="xl">
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>
+                    <ModalCloseButton />
+                </ModalHeader>
+                <ModalBody>
+                    <Text>{ message }</Text>
+                </ModalBody>
+                <ModalFooter>
+                    <HStack>
+                        <Button colorScheme="red" width="100px">Delete</Button>
+                        <Button onClick={onClose} width="100px">Cancel</Button>
+                    </HStack>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
+}
+
+export default DeletePrompt;
